9d74bf14b546411b66df3a1db1a4640b71189ed6
powdhar <benjaminpowdhar@gmail.com>
2022-07-03 21:04:43 +0200

    Legge til Footer og gjøre endringer på stil

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -4,14 +4,17 @@ import React, { Component } from 'react';
 const links = [{
     "label": 'Send meg en e-post',
     "link": 'mailto:benjaminpowdhar@gmail.com>benjaminpowdhar@gmail.com',
+    "target": '',
     "linkText": 'benjaminpowdhar@gmail.com'
 }, {
     "label": 'Nå ut til meg på LinkedIn',
-    "link": 'https://www.linkedin.com/in/benjamin-powdhar/" target="_blank" rel="noopener noreferrer">linkedin.com/in/benjamin-powdhar/',
+    "link": 'https://www.linkedin.com/in/benjamin-powdhar/' ,
+    "target": 'target="_blank" rel="noopener noreferrer">linkedin.com/in/benjamin-powdhar/',
     "linkText": 'linkedin.com/in/benjamin-powdhar/'
 }, {
     "label": "Ta en titt på CV'en min",
-    "link": 'https://drive.google.com/file/d/19pWEYM4S6o87GE7uqEpgxqVmFIvKauHm/view" target="_blank" rel="noopener noreferrer"></i>Last ned som PDF',
+    "link": 'https://drive.google.com/file/d/19pWEYM4S6o87GE7uqEpgxqVmFIvKauHm/view',
+    "target": 'target="_blank" rel="noopener noreferrer>',
     "linkText": 'Last ned som PDF'
 }]
 
@@ -19,8 +22,8 @@ export default class Contact extends Component {
     render() {
         return(
             <React.Fragment>
-              {links.map(({ label, link, linkText }) => (
-                <><div className='Contact' key="key"><p key={label}>{label}</p><a key={link} href={link} key1={linkText}>{linkText}</a></div></>
+              {links.map(({ label, link, linkText, target }) => (
+                <><div className='Contact' key="key"><p key={label}>{label}</p><a key={link} href={link} keylinkText={linkText} keyTarget={target} target={target}>{linkText}</a></div></>
               ))}
             </React.Fragment>
         )
